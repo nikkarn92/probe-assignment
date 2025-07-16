cb4a52cd510237215e49652ede6a281b14bbeaef
nikkarn92 <42508516+nikkarn92@users.noreply.github.com>
2025-07-16 22:58:15 +0530

    create shouldAllowMoveWithinBoundaries

--- src/test/java/com/test/assignment/probe_test/model/GridTest.java
+++ src/test/java/com/test/assignment/probe_test/model/GridTest.java
@@ -1,6 +1,14 @@
 package com.test.assignment.probe_test.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GridTest {
 
-    
+    @Test
+    void shouldAllowMoveWithinBoundaries() {
+        Grid grid = new Grid(5, 5);
+        assertTrue(grid.isWithinBounds(4, 4));
+    }
 }
