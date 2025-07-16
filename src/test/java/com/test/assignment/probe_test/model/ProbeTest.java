8f7d4827d27e5050cda3c0586c73878d236ac859
nikkarn92 <42508516+nikkarn92@users.noreply.github.com>
2025-07-16 22:36:52 +0530

    add shouldInitializeProbeAtGivenPositionAndDirection

--- src/test/java/com/test/assignment/probe_test/model/ProbeTest.java
+++ src/test/java/com/test/assignment/probe_test/model/ProbeTest.java
@@ -4,5 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProbeTest {
-    
+    @Test
+    void shouldInitializeProbeAtGivenPositionAndDirection() {
+        Probe probe = new Probe(1, 2, Direction.NORTH);
+        assertEquals(1, probe.getX());
+        assertEquals(2, probe.getY());
+        assertEquals(Direction.NORTH, probe.getDirection());
+    }
 }
