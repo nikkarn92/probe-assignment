75736acfafcdfbb5619f37603365ba9aa17e5a87
nikkarn92 <42508516+nikkarn92@users.noreply.github.com>
2025-07-16 22:31:14 +0530

    Project Structure

---
+++ src/main/java/com/test/assignment/probe_test/ProbeTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.assignment.probe_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbeTestApplication.class, args);
+	}
+
+}
