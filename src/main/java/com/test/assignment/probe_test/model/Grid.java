2f7407cecd3ee4d1d127735e1d1c2473b5f78a65
nikkarn92 <42508516+nikkarn92@users.noreply.github.com>
2025-07-16 23:00:11 +0530

    Update Grid.java

--- src/main/java/com/test/assignment/probe_test/model/Grid.java
+++ src/main/java/com/test/assignment/probe_test/model/Grid.java
@@ -1,4 +1,15 @@
 package com.test.assignment.probe_test.model;
 
 public class Grid {
+    private final int width;
+    private final int height;
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean isWithinBounds(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
 }
